dc7a8f546252b33669ec89b1b8ea50916c3e8486
Shafayat Fahim <theanonto@outlook.com>
2025-07-01 17:11:47 +0600

    add func

--- lib/function.dart
+++ lib/function.dart
@@ -9,12 +9,20 @@ void main() {
   myC.printName("Shafayat");
 
   myC.printName("Fahim");
+
+  print(myC.add(10, 20));
+
+  print(myC.add(50, 15));
 }
 
 class myClass {
 
   void printName(String name) { //Declaration
     print(name); //Definition
+  }
 
+  int add(int no1, int no2) {
+    int sum = no1 + no2;
+        return sum;
   }
 }
