b044f94f0568a0f4b1b2153bba2549e1f401f2c4
Shafayat Fahim <theanonto@outlook.com>
2025-07-01 19:14:23 +0600

    list_add

---
+++ lib/dart_list.dart
@@ -0,0 +1,12 @@
+main() {
+  var listA = [10, 20, 30, 40];
+  listA.add(50);
+  print("$listA");
+
+  var names = [];
+  names.add("Khan");
+  names.add("Ahmed");
+  names.add("Rahman");
+
+  print("$names");
+}
