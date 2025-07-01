7f91826824c7f28704711eb25fbb51954fd0ea9c
Shafayat Fahim <theanonto@outlook.com>
2025-07-01 16:16:55 +0600

    dynamic

---
+++ lib/var_vs_dynamic.dart
@@ -0,0 +1,21 @@
+void main() {
+  print("Hello World!");
+
+  String name = "Rex";
+
+  //var
+  var sub = "Math";
+  sub = "Bangla"; // var doesn't allow us to override value type.
+
+  //dynamic
+  var section; // This how it becomes dynamic.
+
+  section = 7;
+  section = "D";
+
+  //dynamic
+  dynamic sec;
+
+  sec = 7;
+  sec = "D";
+}
