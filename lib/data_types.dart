e834184213b2903949c572322f49c254a3c99943
Shafayat Fahim <theanonto@outlook.com>
2025-07-01 15:49:35 +0600

    Assigning value to variable

--- lib/data_types.dart
+++ lib/data_types.dart
@@ -2,8 +2,10 @@ void main() {
   print("Hello World!");
 
   //Declaration of Variable
-  int? a;  //By putting the '?' next to 'int' allows us to store null-value on 'a'
+  int a;  //By putting the '?' next to 'int' allows us to store null-value on 'a'
 
+  //Assign value
+  a = 5;
   print(a);
 
 }
