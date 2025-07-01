81df95ee8d971e7f08a813be395c81ad09ab0bf4
Shafayat Fahim <theanonto@outlook.com>
2025-07-01 15:58:42 +0600

    BigInt

--- lib/data_types.dart
+++ lib/data_types.dart
@@ -15,4 +15,10 @@ void main() {
   String name = "Rex"; //String data type
   print("The name of the user is $name!");
 
+  // Using BigInt to assign large value that int failed to store.
+  BigInt longValue;
+  longValue = BigInt.parse("294573945693482039429385634563495863495723904");
+
+  print(longValue);
+
 }
