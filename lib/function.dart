acabc461db7645895c4fc9f48fe216fece8d78a6
Shafayat Fahim <theanonto@outlook.com>
2025-07-01 16:48:01 +0600

    Function calling

---
+++ lib/function.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Hello World!");
+
+  var myC = myClass();
+  myC.printName(); // Func calling
+}
+
+class myClass {
+
+  void printName() { //Declaration
+    print("Rex"); //Definition
+
+  }
+}
