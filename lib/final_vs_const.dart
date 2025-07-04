1d22fd58b5c509ae0e5a8a9e83067a3d2b6decc4
Shafayat Fahim <theanonto@outlook.com>
2025-07-04 21:53:23 +0600

    update

---
+++ lib/final_vs_const.dart
@@ -0,0 +1,17 @@
+void main() {
+  print("Hello World!");
+
+  final String name;
+  name = "Rex";
+
+  const address = "Dhaka";
+
+  final player = [
+    "John", "Daniel", "Elon", "Parker"
+  ];
+  print(player);
+  player.add("Jonathon");
+  print(player);
+  player.remove("Elon");
+  print(player);
+}
