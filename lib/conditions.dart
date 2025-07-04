d65fed499b498036a4de26607f2bf97fb7c66fbd
Shafayat Fahim <theanonto@outlook.com>
2025-07-04 22:22:04 +0600

    else if

--- lib/conditions.dart
+++ lib/conditions.dart
@@ -9,4 +9,14 @@ void main() {
   } else {
     print("a is lesser than b");
   }
+
+  var x = 1000;
+
+  if (x==100) {
+    print("x is 100");
+  } else if (x>100) {
+    print("x is greater than 100");
+  } else {
+    print("x is less than 100");
+  }
 }
