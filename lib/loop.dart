962f6c54d194c9672251a3e2a32b8e6de0c2553a
Shafayat Fahim <theanonto@outlook.com>
2025-07-05 10:46:19 +0600

    do-while

--- lib/loop.dart
+++ lib/loop.dart
@@ -3,4 +3,9 @@ void main() {
   for (int i=0; i<10; i++) {
     print("Hello World!");
   }
+
+  int a = 100;
+  do {
+    print("Condition is working!");
+  } while (a<50); //do-while will print the task at least once even if the condition is not true.
 }
