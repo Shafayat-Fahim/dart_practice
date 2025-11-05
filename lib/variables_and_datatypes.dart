5368ae7b9336a29e1dca58c83044f28ba7e548eb
Shafayat Fahim <theanonto@outlook.com>
2025-11-05 23:22:28 +0600

    BigInt

--- lib/variables_and_datatypes.dart
+++ lib/variables_and_datatypes.dart
@@ -6,5 +6,9 @@ void main() {
   a = 17;
   print(a);
 
+  BigInt longValue;
+  longValue = BigInt.parse('2457294572475245724572457245723457');
+  print(longValue);
+
   String name = "Rex"; //Inline declaration
 }
