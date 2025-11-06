a5af28ddc6d1c872269a3ea949aeecc5b575bc9b
Shafayat Fahim <theanonto@outlook.com>
2025-11-06 21:03:59 +0600

    var

---
+++ lib/var_vs_dynamic.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Zia";
+
+  // var
+
+  var city = "Dhaka";
+  print("This is $city");
+
+  // city = 7; 'We can't assign different data type from the first input type
+
+  city = "Sylhet";
+  print("This is $city");
+}
