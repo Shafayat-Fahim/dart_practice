8f4bc29f22d113baeb3721342af263e4927751d0
Shafayat Fahim <theanonto@outlook.com>
2025-11-06 21:20:35 +0600

    dynamic

--- lib/var_vs_dynamic.dart
+++ lib/var_vs_dynamic.dart
@@ -6,8 +6,26 @@ void main() {
   var city = "Dhaka";
   print("This is $city");
 
-  // city = 7; 'We can't assign different data type from the first input type
+  // city = 7; 'We can't assign different data type from the first input type. This happens only when we assign value during the declaration
 
   city = "Sylhet";
   print("This is $city");
+
+  // dynamic
+
+  var section;
+  section = "X";
+  print("This is section $section. Welcome!");
+
+  section = 7;
+  print("This is section $section. Welcome!");
+
+  // Just because we did not assigned any value to the variable during declaration, it has become 'dynamic'. 'var' or 'dynamic' doesn't mean anything, all that matters is whether we assigned value during the declaration or not!
+
+  dynamic number;
+  number = "Seven";
+  print("My favorite number is $number");
+
+  number = 7;
+  print("My favorite number is $number");
 }
