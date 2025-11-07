66fda4e98c0f6737baaa2fe20fb2ddc49e297646
Shafayat Fahim <theanonto@outlook.com>
2025-11-07 22:58:56 +0600

    Create functions.dart

---
+++ lib/functions.dart
@@ -0,0 +1,12 @@
+void main() {
+  print("Long Live Bangladesh");
+  
+  var myC = myClass();
+  myC.printName(); // Function calling
+}
+
+class myClass{
+  void printName() { //Declaration
+    print("Ziaur Rahman"); //Definition
+  }
+}
