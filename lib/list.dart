d83e15438579cb51b4e059a037d90709319ef604
Shafayat Fahim <theanonto@outlook.com>
2025-11-08 21:50:53 +0600

    insertAll

--- lib/list.dart
+++ lib/list.dart
@@ -4,11 +4,15 @@ void main() {
   listMarks.add(40);
   listMarks.insert(3, 100); //inserts value to the declared index
 
+  var notableLeaders = ["Vasani", "Sohrawardi", "Jinnah", "Gandhi"];
+
   var names = []; //empty list
   names.add("Zia");
   names.add("Khaleda");
   names.add("Tarique");
   names.addAll(listMarks); //'addAll' adds an existing list to the list
+  names.insertAll(3, notableLeaders); //inserts a full list in the position of declared index
+
 
   print(listMarks);
   print(names);
