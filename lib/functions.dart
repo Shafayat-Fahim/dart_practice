a4df9f45c1e5580e8f086254ed058e5a7b97a36a
Shafayat Fahim <theanonto@outlook.com>
2025-11-08 11:47:43 +0600

    addition

--- lib/functions.dart
+++ lib/functions.dart
@@ -3,10 +3,24 @@ void main() {
   
   var myC = myClass();
   myC.printName(); // Function calling
+
+  myC.printCountry("Bangladesh");
+  myC.printCountry("Argentina");
+
+  print(myC.addition(7, 10));
 }
 
 class myClass{
   void printName() { //Declaration
     print("Ziaur Rahman"); //Definition
+  }
+
+  void printCountry(String country) {
+    print(country);
+  }
+
+  int addition(int no1, int no2) {
+    int sum = no1+no2;
+    return sum;
   }
 }
