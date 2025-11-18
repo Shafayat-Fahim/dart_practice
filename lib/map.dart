194e373f6c68910931979b7fcae84ede3f613f54
Shafayat Fahim <theanonto@outlook.com>
2025-11-18 23:10:17 +0600

    isNotEmpty

--- lib/map.dart
+++ lib/map.dart
@@ -1,16 +1,25 @@
 void main() {
-  var map_name = {
-    "Key1" : "Value1",
-    "Key2" : 17,
-    "Key3" : 7.5,
-    "Key4" : true
-  };
+  // var map_name = {
+  //   "Key1" : "Value1",
+  //   "Key2" : 17,
+  //   "Key3" : 7.5,
+  //   "Key4" : true
+  // };
+  //
+  // print(map_name);
+  // print(map_name["Key2"]); //map value finding by key
+  // print(map_name["Key5"]); //returns 'null' for not found key
+  // print(map_name["key2"]); //returns 'null' because Dart key is case sensitive
+  //
+  // map_name["Key1"] = "Rex"; //overwrites the key value
+  // print(map_name["Key1"]);
 
-  print(map_name);
-  print(map_name["Key2"]); //map value finding by key
-  print(map_name["Key5"]); //returns 'null' for not found key
-  print(map_name["key2"]); //returns 'null' because Dart key is case sensitive
 
-  map_name["Key1"] = "Rex"; //overwrites the key value
-  print(map_name["Key1"]);
+  var mapEmployee = Map();
+  mapEmployee["Name"] = "Rex";
+  mapEmployee["ID"] = 001;
+  mapEmployee["YearsOfExp"] = 7;
+  mapEmployee["City"] = "Dhaka";
+  
+  print(mapEmployee.isNotEmpty); //checks if the map is not empty
 }
