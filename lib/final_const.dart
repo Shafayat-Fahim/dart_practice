36ddf83b080b1c1e605ab1175acab2d69ea3d6d2
Shafayat Fahim <theanonto@outlook.com>
2025-11-20 10:09:10 +0600

    inline final variable

--- lib/final_const.dart
+++ lib/final_const.dart
@@ -1,8 +1,10 @@
 void main() {
   print("Why so serious?");
 
-  final name = "Rex";
-  name = "Rox"; //can not assign value to the final variable after assigning a value
+  // final name = "Rex"; //inline final variable
+  // name = "Rox"; //can not assign value to the final variable after assigning a value
+  //
+  // print(name);
 
-  print(name);
+
 }
